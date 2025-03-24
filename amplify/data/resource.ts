32f75d7ab67a83422c94649176cf47ac3d793b48
serdjukow <serduykovs@yandex.ru>
2025-03-24 17:27:05 +0100

    Delete, update functions

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend"
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -7,24 +7,25 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+    Todo: a
+        .model({
+            content: a.string(),
+            completed: a.boolean(),
+        })
+        .authorization((allow) => [allow.publicApiKey()]),
+})
 
-export type Schema = ClientSchema<typeof schema>;
+export type Schema = ClientSchema<typeof schema>
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+    schema,
+    authorizationModes: {
+        defaultAuthorizationMode: "apiKey",
+        apiKeyAuthorizationMode: {
+            expiresInDays: 30,
+        },
     },
-  },
-});
+})
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,77 @@
-"use client";
+"use client"
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+import { useState, useEffect } from "react"
+import { generateClient } from "aws-amplify/data"
+import type { Schema } from "@/amplify/data/resource"
+import "./../app/app.css"
+import { Amplify } from "aws-amplify"
+import outputs from "@/amplify_outputs.json"
+import "@aws-amplify/ui-react/styles.css"
 
-Amplify.configure(outputs);
+Amplify.configure(outputs)
 
-const client = generateClient<Schema>();
+const client = generateClient<Schema>()
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([])
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
+    function listTodos() {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        })
+    }
 
-  useEffect(() => {
-    listTodos();
-  }, []);
+    useEffect(() => {
+        listTodos()
+    }, [])
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+    function createTodo() {
+        const content = window.prompt("Todo content")
+        if (content) {
+            client.models.Todo.create({ content, completed: false })
+        }
+    }
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
+
+    function updateTodo(id: string) {
+        const newContent = window.prompt("Update todo content")
+        if (newContent) {
+            client.models.Todo.update({ id, content: newContent })
+        }
+    }
+
+    function toggleCompleted(id: string, completed: boolean) {
+        client.models.Todo.update({ id, completed: !completed })
+    }
+
+    return (
+        <main>
+            <h1>My todos</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id} style={{ display: "flex", justifyContent: "space-between", gap: "1em", alignItems: "center" }}>
+                        <input type="checkbox" checked={todo.completed} onChange={() => toggleCompleted(todo.id, todo.completed)} />
+                        {todo.content}
+                        <div style={{ display: "flex", justifyContent: "space-between", gap: ".5em" }}>
+                            <button style={{ backgroundColor: "green" }} onClick={() => updateTodo(todo.id)}>
+                                Edit
+                            </button>
+                            <button style={{ backgroundColor: "tomato" }} onClick={() => deleteTodo(todo.id)}>
+                                Delete
+                            </button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br />
+                <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">Review next steps of this tutorial.</a>
+            </div>
+        </main>
+    )
 }
